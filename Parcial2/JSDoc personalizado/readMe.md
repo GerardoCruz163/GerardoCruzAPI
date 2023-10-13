56a02fb2a615b949922f12cda674825701a385fe
GerardoCruz163 <gerarojo243@gmail.com>
2023-10-13 08:55:00 -0600

    jsDoc Personalizado

---
+++ Parcial2/JSDoc personalizado/readMe.md
@@ -0,0 +1,14 @@
+# modulo Prueba
+
+Ejercicio de creacion de mondulos de javascript
+y publicacion en el registro de npm
+
+Instalacion del modulo:
+```bash
+    $ npm install JS
+```
+
+Uso del modulo:
+```js
+    const JS=require('JS');
+    let frase= JS.obtieneFrase(n);
---
+++ Parcial2/JSDoc personalizado/src/index.js
@@ -0,0 +1,18 @@
+let frases=[
+    "frase 1",
+    "frase 2",
+    "frase 3"
+]
+
+/**
+ * 
+ * @param {*} indice 
+ * @returns HOLA PERRI
+ */
+function obtieneFrase(indice){
+    return frases(indice);
+}
+
+module.exports.obtieneFrase=obtieneFrase;
+
+console.log(module);
